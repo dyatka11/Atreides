3d33106e0e77e66a17b1ad40b41bab344eb42ae7
Lisa <dyatka11@gmail.com>
2024-05-30 15:46:57 -0500

    Task1 is done!

--- src/main/java/Lisa/Task1.java
+++ src/main/java/Lisa/Task1.java
@@ -1,4 +1,18 @@
 package Lisa;
 
 public class Task1 {
+    public static void main(String[] args) {
+        String str1 = "(]";
+        String str = "({}){{[{}]}}[][]";
+        System.out.println(task1(str));
+    }
+    public static boolean task1(String str){
+        for (int i = 0; i <str.length()+1; i++) {//I don't know why length must be +1,but it works
+            // this way
+            str = str.replaceAll("\\[\\]","");
+            str = str.replaceAll("\\(\\)","");
+            str = str.replaceAll("\\{\\}","");
+        }
+        return str.isEmpty();
+    }
 }
