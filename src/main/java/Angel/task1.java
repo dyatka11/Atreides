7c9f6c7915f4c2586db7977f4059082c6dcc0578
Lisa <dyatka11@gmail.com>
2024-05-30 12:40:02 -0500

    Project created

--- src/main/java/Angel/task1.java
+++ src/main/java/Angel/task1.java
@@ -1,4 +1,11 @@
 package Angel;
 
 public class task1 {
+    /**
+     * • Floyd’s triangle is the right angled triangle consists of natural numbers in the following fashion.
+     * Floyd's Triangle :
+     * • 12 3 456
+     * 7 8 9 10
+     * 11 12 13 14 15
+     */
 }
